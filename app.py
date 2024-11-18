69f8684eb9780689a7b88f504ac827dc9044f6d0
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2024-11-19 01:48:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from google.oauth2 import service_account
 from src.client import GoogleAPIClient
 from src.processors import XParkyProcessor
 from dotenv import load_dotenv
